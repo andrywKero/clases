a95de087e55e75d7cece6a6cc698508214f8ca7f
Andres <keroby@zoho.com>
2019-06-15 15:18:59 -0500

    Modulo 1

---
+++ ModuloI/src/main/java/co/com/tdea/InOutPut.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.com.tdea;
+
+/**
+ * @author andres
+ *
+ */
+public class InOutPut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
